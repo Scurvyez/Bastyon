811297e50669a38a2e379d949623ce86293aa8f5
Carnagion <59148707+Carnagion@users.noreply.github.com>
2021-06-03 13:45:42 +0200

    Add files via upload

---
+++ Source/Bastyon/BastyonModSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+
+namespace Bastyon
+{
+    public class BastyonModSettings : ModSettings
+    {
+        public List<string> disabledBastyonAnimals = new List<string>();
+
+        public override void ExposeData()
+        {
+            Scribe_Collections.Look(ref disabledBastyonAnimals, "disabledBastyonAnimals", LookMode.Value, Array.Empty<object>());
+        }
+    }
+}
