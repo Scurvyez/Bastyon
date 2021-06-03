fd5647012847c294e8a0e7e438b67fc6594607a6
Carnagion <59148707+Carnagion@users.noreply.github.com>
2021-06-03 13:54:53 +0200

    Add files via upload

---
+++ Source/Bastyon/BastDefRemover.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Bastyon
+{
+    public static class BastDefRemover
+    {
+        public static bool Prefix_GenerateImpliedDefs_PreResolve()
+        {
+            BastyonMod.allBastyonAnimals = (from currentDef in DefDatabase<PawnKindDef>.AllDefs
+                                            where currentDef.defName.Contains("Bast_")
+                                            orderby currentDef.defName
+                                            select currentDef).ToList<PawnKindDef>();
+            DefDatabase<PawnKindDef>.AllDefsListForReading.RemoveAll(pawnKindDef => BastyonMod.modSettings.disabledBastyonAnimals.Contains(pawnKindDef.defName));
+            return true;
+        }
+    }
+}
