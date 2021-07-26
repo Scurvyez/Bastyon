998d1e5280c946807308ac02c1050bfd2c110f97
Instakrill <aaron.karlsen@gmail.com>
2021-07-26 15:51:05 +1000

    removed error causing looping of bahlrin raids

--- 1.2/Source/Bastyon/IncidentWorker_RaidCustom.cs
+++ 1.2/Source/Bastyon/IncidentWorker_RaidCustom.cs
@@ -294,7 +294,7 @@ namespace Bastyon
             {
 				parms.raidStrategy.Worker.MakeLords(parms, list);
             }
-			LessonAutoActivator.TeachOpportunity(ConceptDefOf.EquippingWeapons, OpportunityType.Critical);
+			/*LessonAutoActivator.TeachOpportunity(ConceptDefOf.EquippingWeapons, OpportunityType.Critical);
 			if (!PlayerKnowledgeDatabase.IsComplete(ConceptDefOf.ShieldBelts))
 			{
 				for (int j = 0; j < list.Count; j++)
@@ -306,6 +306,7 @@ namespace Bastyon
 					}
 				}
 			}
+			*/
 			Find.TickManager.slower.SignalForceNormalSpeedShort();
 			Find.StoryWatcher.statsRecord.numRaidsEnemy++;
 			Log.Message("2 parms.raidStrategy: " + parms.raidStrategy);
