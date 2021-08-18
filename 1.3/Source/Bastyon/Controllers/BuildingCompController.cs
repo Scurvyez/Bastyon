9d4c5d6414563dfc96517c229f23b60bf87a22b1
Aaron <aaron.karlsen@gmail.com>
2021-08-18 17:08:56 +1000

    adding more logic to how repellant nodes connect

--- 1.3/Source/Bastyon/Controllers/BuildingCompController.cs
+++ 1.3/Source/Bastyon/Controllers/BuildingCompController.cs
@@ -23,29 +23,69 @@ namespace Bastyon
             range = def.specialDisplayRadius;
             GenDraw.DrawRadiusRing(center, this.range);
 
-
-
             //find nearest second building and draw a line between them.
             List<Thing> forCell = Find.CurrentMap.listerBuldingOfDefInProximity.GetForCell(center, this.range, def);
-            if(forCell != null)
+            List<Dictionary<string, object>> ConnectorPairs = new List<Dictionary<string, object>>();
+
+            foreach (Thing node in forCell)
             {
-                if (forCell.Count > 1)
+                ConnectorPairs.Add(new Dictionary<string, object> {
+                    {"CurrentNode", node.Position },
+                    {"xVector", forCell.OrderBy(xValue => Math.Abs(node.Position.x - xValue.Position.x)).First().Position},
+                    { "zVector", forCell.OrderBy(zValue => Math.Abs(node.Position.z - zValue.Position.z)).First().Position},
+                    { "xConnection", false},
+                    { "zConnection", true}
+                });
+            }
+
+            if (forCell.Count > 2)
+            {
+                for (int i = 0; i < 2; i++)
                 {
-                    for (int i = 0; i < 2; i++)
+                    Dictionary<string, object> NodeConnectors = ConnectorPairs[i];
+                    bool xConnector = Convert.ToBoolean(NodeConnectors["xConnection"]);
+                    bool zConnector = Convert.ToBoolean(NodeConnectors["zConnection"]);
+                    var xNode = NodeConnectors["xVector"];
+                    var zNode = NodeConnectors["zVector"];
+                    var currNode = NodeConnectors["CurrentNode"];
+
+                    foreach (Dictionary<string, object> nodeConnector in ConnectorPairs)
                     {
-                        GenDraw.DrawLineBetween(GenThing.TrueCenter(center, Rot4.North, def.size, def.Altitude), forCell[i].TrueCenter(), SimpleColor.Green, 0.2f);
+                        if (xConnector == false)
+                        {
+                            GenDraw.DrawLineBetween(GenThing.TrueCenter(center, Rot4.North, def.size, def.Altitude), (Vector3)xNode, SimpleColor.Green, 0.2f);
+                            ConnectorPairs[i]["xConnection"] = true;
+                            foreach (Dictionary<String, object> x in ConnectorPairs)
+                            {
+                                if(x["CurrentNode"] == ConnectorPairs[i]["xConnection"])
+                                {
+                                    x["xConnection"] = true;
+                                    break;
+                                }
+                            }
+                        }
+                        if (zConnector == false)
+                        {
+                            GenDraw.DrawLineBetween(GenThing.TrueCenter(center, Rot4.North, def.size, def.Altitude), (Vector3)zNode, SimpleColor.Green, 0.2f);
+                            ConnectorPairs[i]["zConnection"] = true;
+                            foreach (Dictionary<String, object> z in ConnectorPairs)
+                            {
+                                if (z["CurrentNode"] == ConnectorPairs[i]["zConnection"])
+                                {
+                                    z["zConnection"] = true;
+                                    break;
+                                }
+                            }
+                        }
                     }
                 }
-                else if (forCell.Count == 1)
-                {
-                    GenDraw.DrawLineBetween(GenThing.TrueCenter(center, Rot4.North, def.size, def.Altitude), forCell[0].TrueCenter(), SimpleColor.Green, 0.2f);
-                }
-            } 
+            }
         }
+
 
         public float range;
     }
-
+    
     public class LinkBuildingNodeController
     {
         
