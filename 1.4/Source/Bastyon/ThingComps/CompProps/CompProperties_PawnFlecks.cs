f1cbd7f290d235296980d7149789a2842c9e440d
Steve <stevenzybertusn@gmail.com>
2022-12-30 19:27:35 -0500

    wisp testing, graphics code update

---
+++ 1.4/Source/Bastyon/ThingComps/CompProps/CompProperties_PawnFlecks.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+using Verse;
+using Verse.AI;
+using UnityEngine;
+
+namespace Bastyon
+{
+    public class CompProperties_PawnFlecks : CompProperties
+    {
+        public FleckDef fleck = null;
+        public int burstCount = 1;
+        public Vector3 offset = new Vector3(0, 0, 0);
+        public Color colorA = Color.white;
+        public Color colorB = Color.white;
+        public FloatRange scale;
+
+        public bool staggeredTimings = false;
+        public int staggeredTimingInt = 0;
+
+        public IntRange randomizedTimingRange;
+
+        public CompProperties_PawnFlecks() => compClass = typeof(Comp_PawnFlecks);
+    }
+}
---
+++ 1.4/Source/Bastyon/ThingComps/Comp_PawnFlecks.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+using RimWorld;
+using Verse;
+using Verse.AI;
+using UnityEngine;
+
+namespace Bastyon
+{
+    public class Comp_PawnFlecks : ThingComp
+    {
+        public CompProperties_PawnFlecks Props => (CompProperties_PawnFlecks)props;
+        private Color EmissionColor => Color.Lerp(Props.colorA, Props.colorB, Rand.Value);
+
+        public override void CompTick()
+        {
+            base.CompTick();
+
+            if (Props.staggeredTimings)
+            {
+                if (parent.IsHashIntervalTick(Props.staggeredTimingInt * Props.randomizedTimingRange.RandomInRange))
+                {
+                    Emit();
+                }
+            }
+            else
+            {
+                Emit();
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void Emit()
+        {
+            Vector3 vel = new Vector3(0.00f, 0.00f, 1.00f); // velocity
+
+            for (int i = 0; i < Props.burstCount; ++i)
+            {
+                FleckCreationData fCD = FleckMaker.GetDataStatic(parent.DrawPos + Props.offset, parent.Map, Props.fleck, Props.scale.RandomInRange);
+                fCD.rotationRate = Rand.RangeInclusive(-240, 240);
+                fCD.instanceColor = new Color?(EmissionColor);
+                fCD.velocitySpeed = Rand.Range(0.1f, 0.8f);
+                fCD.velocity = vel * 3.0f;
+                parent.Map.flecks.CreateFleck(fCD);
+            }
+        }
+    }
+}
